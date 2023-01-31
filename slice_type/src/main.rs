d5cca5e7c112f659ee753d93e7ab450896290bc5
oscar <oscaroceguera@gmail.com>
2023-01-30 22:34:03 -0700

    Added Using structs to structure related data

---
+++ slice_type/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let mut s = String::from("hello world");
+
+    let _world = first_word(&s);
+
+    s.clear();
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
